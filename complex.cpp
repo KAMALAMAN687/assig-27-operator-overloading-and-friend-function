1aecc3d18053e755823639fe3a51b02df0fbd94e
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-11-08 19:40:35 +0530

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -12,7 +12,7 @@ class Complex
     }
     void showdata()
     {
-        cout<<"real= "<<a<<" imaginary = "<<b;
+        cout<<"real= "<<a<<" imaginary = "<<b<<endl;
     }
     
     Complex operator+(Complex c)
@@ -36,18 +36,32 @@ class Complex
         temp3.b=a*e.b+b*e.a;
         return temp3;
     }
+   friend bool operator==(Complex , Complex);
+   
 };
+bool operator==( Complex e, Complex f)
+{
+    if(e.a==f.a&&e.b==f.b)
+    {
+       return true;
+    }
+    else
+    return false;
+           
+}
 int main()
 {
     Complex c1,c2,c3,c4,c5,c6;
     c1.setdata(2,4);
     c2.setdata(5,6);
+    c6.setdata(-14,32);
     c3=c1+c2;
     c4=c1-c2;
     c5=c1*c2;
+    cout<<operator==(c6,c5);
+    cout<<endl;
     c3.showdata();
     c4.showdata();
     c5.showdata();
     return 0;
-
 }
